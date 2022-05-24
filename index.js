ba6a1bb7ec66228260442bece6358ede2fa62b4d
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-05-24 19:14:20 +0600

    MONGODB connected

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
+
+// mongodb section start
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e2lck.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const useProducts = client.db("autoparts").collection("products");
+    } finally {
+        // empty for now
+    }
+}
+
+run().catch(console.dir)
+
 
 // main route api
 app.get('/', async (req, res) => {
